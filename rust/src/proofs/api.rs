b420cdfeb0b4fd4cd9ae8ffdad476ff16343649c
石博文 <sbw@sbw.so>
2020-10-18 19:01:02 +0800

    remove cond

--- rust/src/proofs/api.rs
+++ rust/src/proofs/api.rs
@@ -269,7 +269,7 @@ pub unsafe extern "C" fn fil_seal_pre_commit_phase1(
 
         info!("seal_pre_commit_phase1: start");
 
-        let _guard = wait_cond!("P1".to_string(), 30);
+        //let _guard = wait_cond!("P1".to_string(), 30);
 
         let public_pieces: Vec<PieceInfo> = from_raw_parts(pieces_ptr, pieces_len)
             .iter()
@@ -324,7 +324,7 @@ pub unsafe extern "C" fn fil_seal_pre_commit_phase2(
 
         info!("seal_pre_commit_phase2: start");
 
-        let _guard = wait_cond!("P2".to_string(), 30);
+        //let _guard = wait_cond!("P2".to_string(), 30);
 
         let mut response: fil_SealPreCommitPhase2Response = Default::default();
 
@@ -382,7 +382,7 @@ pub unsafe extern "C" fn fil_seal_commit_phase1(
 
         info!("seal_commit_phase1: start");
 
-        let _guard = wait_cond!("C1".to_string(), 30);
+        //let _guard = wait_cond!("C1".to_string(), 30);
 
         let mut response = fil_SealCommitPhase1Response::default();
 
@@ -440,7 +440,7 @@ pub unsafe extern "C" fn fil_seal_commit_phase2(
 
         info!("seal_commit_phase2: start");
 
-        let _guard = wait_cond!("C2".to_string(), 30);
+        //let _guard = wait_cond!("C2".to_string(), 30);
 
         let mut response = fil_SealCommitPhase2Response::default();
 
